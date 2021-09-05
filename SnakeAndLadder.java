4126b26001736b9da81747daea0e6b1aa9d00d5a
suhridh <ssg.2711@gmail.com>
2021-09-05 12:25:40 +0530

    fixed UC3

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,22 +7,9 @@ public class SnakeAndLadder {
         // --> Generating Random number between desired range.
         // Min value + (int)(Math.random() * ((Max - Min) + 1))
         int position1 = 0;
-        while (position1 < 100) {
-            int dice = 1 + (int) (Math.random() * ((6 - 1) + 1));
-            int options = 1 + (int) (Math.random() * ((3 - 1) + 1));
-//            System.out.println(dice);
-//            System.out.println(options);
-            switch (options) {
-                case 1:
-                    break;
-                case 2:
-                    position1 += dice;
-                    break;
-                case 3:
-                    position1 -= dice;
-            }
-
-        }
-        System.out.println(position1);
+        int dice = 1 + (int) (Math.random() * ((6 - 1) + 1));
+        int options = 1 + (int) (Math.random() * ((3 - 1) + 1));
+        System.out.println(dice);
+        System.out.println(options);
     }
 }
